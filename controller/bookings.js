6dd2ed058b1ca3fea46b711af99acfbf0efb58f9
calvin <puram.calvin@gmail.com>
2020-03-28 06:28:30 +0100

    change stripe url

--- controller/bookings.js
+++ controller/bookings.js
@@ -17,17 +17,15 @@ exports.getCheckoutSessions = catchAsync(async (req, res, next) => {
   //create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `http://localhost:8080/tour/bookings/${req.params.tourId}/${req.user.id}/${tour.price}`,
-    cancel_url: `http://localhost:8080/tour/${tour.slug}`,
+    success_url: `https://tourview.herokuapp.com/tour/bookings/${req.params.tourId}/${req.user.id}/${tour.price}`,
+    cancel_url: `https://tourview.herokuapp.com/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
       {
         name: tour.name,
         description: tour.summary,
-        images: [
-          'https://images.unsplash.com/photo-1542359649-31e03cd4d909?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80'
-        ],
+        images: [`https://tourview.herokuapp.com/img/${tour.images[0]}`],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1

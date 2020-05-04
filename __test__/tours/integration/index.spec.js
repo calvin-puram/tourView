905f059e3f375578ea37fa1c61d3056a9075f631
calvin <puram.calvin@gmail.com>
2020-05-04 14:39:16 +0100

    setup basic test boilerplate

--- __test__/tours/integration/index.spec.js
+++ __test__/tours/integration/index.spec.js
@@ -1,2 +1,27 @@
-import Tours from '../../../models/Tours';
-import connectDB from '../../../config/db';
+/**
+ * @jest-environment node
+ */
+/* eslint-disable no-undef */
+const supertest = require('supertest');
+const mongoose = require('mongoose');
+
+const Tours = require('../../../models/Tours');
+const connectDB = require('../../../config/db');
+
+const app = require('../../../app.js');
+
+const request = () => supertest(app);
+
+describe('tours.js', () => {
+  beforeEach(async () => {
+    await connectDB();
+  });
+
+  afterEach(async () => {
+    await mongoose.connection.close();
+  });
+
+  it('should return true', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- config/db.js
+++ config/db.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 const chalk = require('chalk');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true
     });
 
-    console.log(
-      chalk.yellowBright(`connected to DB on host: ${conn.connection.host}`)
-    );
+    // console.log(
+    //   chalk.yellowBright(`connected to DB on host: ${conn.connection.host}`)
+    // );
   } catch (err) {
     console.log(chalk.red(`mongoDB error: ${err.message}`));
   }
